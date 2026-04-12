4027099cc8bb78a713d899d21ff177b9e0b1bac1
Dima Shirkov <dimashirkov@list.ru>
2026-04-12 13:11:56 +0300

    chore: del context

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,14 @@
-import { ListPostsProvider } from "../../context/list-posts-provider";
 import { useGetListPosts } from "../../hooks";
 import { ListPosts } from "../list-posts";
+import styles from "./app.module.css";
 
 export function App() {
   const [listPostsDTO, isLoading] = useGetListPosts();
 
   return (
-    <ListPostsProvider>
-      {isLoading && <h2>Loading ...</h2>}
+    <>
+      {isLoading && <h2 className={styles.title}>Loading ...</h2>}
       {listPostsDTO.length > 0 && <ListPosts listPosts={listPostsDTO} />}
-    </ListPostsProvider>
+    </>
   );
 }
--- src/context/index.ts
+++
@@ -1,5 +0,0 @@
-export { ListPostsProvider as PaginationProvider } from "./list-posts-provider";
-export {
-  useListPosts as useCurrentPage,
-  useSetListPosts as useSetCurrentPage,
-} from "./list-posts-context";
