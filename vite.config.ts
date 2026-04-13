7a0209f54a41e0565d314d5724d1d50e8085a520
Dima Shirkov <dimashirkov@list.ru>
2026-04-13 08:51:03 +0300

    build: marked base path for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import readableClassnames from 'vite-plugin-readable-classnames'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import readableClassnames from "vite-plugin-readable-classnames";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    readableClassnames() 
-  ],
-})
+  base: "/",
+  plugins: [react(), readableClassnames()],
+});
